2f89c3e2b2ad93be8f28450b3b93a5192ccbf69b
Mahalakshmi Mothilal <mahalakshmim@stg.com>
2023-03-23 15:57:32 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule}from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ComponentComponent } from './component/component.component';
+import{FormGroup,FormControl}from '@angular/forms';
+import { JwttokenComponent } from './jwttoken/jwttoken.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    JwttokenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
